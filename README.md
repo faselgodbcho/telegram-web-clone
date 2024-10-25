8ce52ca9ae888b308a003a0bea59c8c428abafb7
Fasel Godbcho <faselgodbcho@gmail.com>
2024-10-25 11:05:27 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Telegram Web Clone
+
+- This is a minimal version of the [telegram web application](https://web.telegram.org) version.
+- View live on ...
---
+++ src/config/firebase.config.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+
+// firebase configuration object
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "telegram-web-app-7f668.firebaseapp.com",
+  projectId: "telegram-web-app-7f668",
+  storageBucket: "telegram-web-app-7f668.appspot.com",
+  messagingSenderId: "367614671098",
+  appId: "1:367614671098:web:22c3c866eca551ad47f143",
+  measurementId: "G-XP8CZ9S0EQ",
+};
+
+// init app
+const app = initializeApp(firebaseConfig);
+
+// init services
+const auth = getAuth(app);
+
+// connect to local emulators
+if (import.meta.env.DEV) {
+  connectAuthEmulator(auth, "");
+}
+
+// export services
+export { auth };
