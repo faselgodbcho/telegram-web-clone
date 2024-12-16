036ddaf56907055972b3b303be22c75e68c21c7a
Fasel Godbcho <faselgodbcho@gmail.com>
2024-12-16 04:55:54 -0300

    Setup appwrite auth client.

---
+++ src/config/appwrite/config.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+  endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+  projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
+  secretKey: process.env.NEXT_APPWRITE_KEY!,
+};
