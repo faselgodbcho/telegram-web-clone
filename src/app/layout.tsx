e528a792f31de5f1380dc2900e7dddd556bf1b38
Fasel Godbcho <faselgodbcho@gmail.com>
2024-12-21 04:00:10 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   title: "Telegram Web Clone",
   description:
     "A web application that replicates the features of Telegram, allowing users to chat in real time.",
-  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
+  metadataBase: new URL("https://telegram-web-clone-fasel.vercel.app"),
   openGraph: {
     title: "Telegram Web Clone",
     description: "A minimalistic telegram web clone",
