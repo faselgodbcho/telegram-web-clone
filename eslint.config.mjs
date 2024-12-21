94f869bb89147351a8696d1f3c69b9d37b821f51
Fasel Godbcho <faselgodbcho@gmail.com>
2024-12-21 03:49:57 -0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,8 +11,8 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", rules: {
-      '@typescript-eslint/no-unused-vars': 'off',
-    },),
+      '@typescript-eslint/no-unused-vars': 'off'
+    })
 ];
 
 export default eslintConfig;
