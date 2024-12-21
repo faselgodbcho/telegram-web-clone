b4b2db752b8f7155a692429894d8a3d48dd65076
Fasel Godbcho <faselgodbcho@gmail.com>
2024-12-21 04:13:27 -0300

    Update layout.tsx (fix the hundreth bug)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,13 +12,13 @@ export const metadata: Metadata = {
   title: "Telegram Web Clone",
   description:
     "A web application that replicates the features of Telegram, allowing users to chat in real time.",
-  metadataBase: new URL("https://telegram-web-clone-mu.vercel.app/assets/images/telegram-open-graph-image.avif"),
+  metadataBase: new URL("https://telegram-web-clone-mu.vercel.app"),
   openGraph: {
     title: "Telegram Web Clone",
     description: "A minimalistic telegram web clone",
     images: [
       {
-        url: "/assets/images/telegram-open-graph-image.avif",
+        url: "https://telegram-web-clone-mu.vercel.app/assets/images/telegram-open-graph-image.avif",
         width: 1200,
         height: 630,
         alt: "Telegram Web Logo",
